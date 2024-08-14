22564c81b0268b06bb167dce6c979d8f60f0c4be
burppp <1483543906@qq.com>
2024-08-14 11:35:50 +0800

    init

---
+++ tracking_car/MDK-ARM/tracking_car/startup_stm32f429xx.d
@@ -0,0 +1,1 @@
+tracking_car\startup_stm32f429xx.o: startup_stm32f429xx.s
---
+++ tracking_car/MDK-ARM/tracking_car/stm32f4xx_ll_adc.d
@@ -0,0 +1,1 @@
+tracking_car\stm32f4xx_ll_adc.o: C:/Users/Lumos/STM32Cube/Repository/STM32Cube_FW_F4_V1.28.0/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_ll_adc.c
